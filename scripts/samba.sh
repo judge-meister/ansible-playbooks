aa7b0e4fee9bf094b2cbfd916fc46908997ecd20
Ian Judge <ianjudge1969@gmail.com>
2021-09-29 22:08:13 +0100

    update samba vm config

--- scripts/samba.sh
+++ scripts/samba.sh
@@ -11,13 +11,22 @@ set_hostname
 # install packages
 apt install samba nfs-common vim git curl python3-apt qemu-guest-agent
 
-cp smb.conf /etc/samba/smb.conf
+#cp smb.conf /etc/samba/smb.conf
+
+cp /etc/samba/smb.conf smb.conf.orig
+
+sed -e '/^\[global\].*/r smb.conf.d/smb.global.conf' \
+    -e '$r smb.conf.d/smb.shares.conf' \
+    smb.conf.orig > smb.conf.new
+
+cp smb.conf.new /etc/samba/smb.conf
 
 echo -e "\nCreate samba password for user judge"
 smbpasswd -a judge
 
+
 # do sudoers
-do_sudo
+#do_sudo
 
 # ---------------------------------------------------------------------
 # fstab - setup mount points and mounting of /zdata and /zvideos from proxmox (192.168.0.30)
